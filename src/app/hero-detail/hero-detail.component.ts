e192dcae562750d484f8b36ae4b792b1ffc4e6b7
Florian <florian.guillet@ecole-isitech.fr>
2018-10-18 16:00:52 +0200

    TrueRootingCommit
    AngularTuto Correction 18/10/2018

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit,} from '@angular/core';
 import { Location } from '@angular/common';
 import {ActivatedRoute} from '@angular/router';
 
@@ -11,24 +11,25 @@ import { HeroService} from '../hero.service';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-  @Input() hero: Hero;
+  hero: Hero;
+
   constructor(
     private route: ActivatedRoute,
     private heroService: HeroService,
     private location: Location
+  ) {}
 
-  ) { }
-
-  ngOnInit(): void{
+  ngOnInit(): void {
     this.getHero();
   }
+
   getHero(): void {
     const id = +this.route.snapshot.paramMap.get('id');
     this.heroService.getHero(id)
       .subscribe(hero => this.hero = hero);
   }
+
   goBack(): void {
     this.location.back();
   }
-
 }
